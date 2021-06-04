440e25d4e261d49280127c5607a6ed747269ff32
Stanislas Bernard <bernardstanislas@gmail.com>
2021-06-05 00:34:02 +0200

    :label: Type DGFIP return

--- src/domain/application/dgfip.dto.ts
+++ src/domain/application/dgfip.dto.ts
@@ -3,4 +3,32 @@ export type DGFIPInput = {
   taxNoticeNumber: string;
 };
 
-export type DGFIPOutput = {};
+type Declarant = {
+  nom: string;
+  nomNaissance: string;
+  prenoms: string;
+  dateNaissance: Date;
+};
+
+export type DGFIPOutput = {
+  declarant1: Declarant;
+  declarant2: Declarant;
+  dateRecouvrement: Date;
+  dateEtablissement: Date;
+  nombreParts: number;
+  situationFamille: string;
+  nombrePersonnesCharge: number;
+  revenuBrutGlobal: number;
+  revenuImposable: number;
+  impotRevenuNetAvantCorrections: number;
+  montantImpot: number;
+  revenuFiscalReference: number;
+  foyerFiscal: {
+    annee: number;
+    adresse: string;
+  };
+  anneeImpots: number;
+  anneeRevenus: number;
+  erreurCorrectif: string;
+  situationPartielle: string;
+};
