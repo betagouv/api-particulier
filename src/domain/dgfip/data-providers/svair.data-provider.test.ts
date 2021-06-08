985d9e95d5436cf44d5f910b07472e4821276a88
Stanislas Bernard <bernardstanislas@gmail.com>
2021-06-08 16:29:30 +0200

    :recycle: Mock network call for view state

--- src/domain/dgfip/data-providers/svair.data-provider.test.ts
+++ src/domain/dgfip/data-providers/svair.data-provider.test.ts
@@ -1,12 +1,21 @@
 import {SvairDataProvider} from './svair.data-provider';
+import axios from 'axios';
+// eslint-disable-next-line node/no-unpublished-import
+import MockAdapter from 'axios-mock-adapter';
 
 describe('Svair data provider', () => {
   const svairDataProvider = new SvairDataProvider();
+  const mock = new MockAdapter(axios);
 
   it('loads the JSF view state', async () => {
-    const viewState = await svairDataProvider.getViewState();
-    expect(viewState).toMatchInlineSnapshot(
-      '"RxJe/1JKTJSr3aiM3H9DqZq0DrwqEXsY7Rw4eLRgEBsCF1IALJGqVgWTaQkiKbbdcGDWW774BWUCa/+j2CDznhw1/3bxJteY6ZCui66yNevhkej4xuyrFMte5KQnKORt9JZrOQ=="'
-    );
+    const expectedViewState = 'the-view-state';
+    mock
+      .onGet()
+      .reply(
+        200,
+        `<input name="javax.faces.ViewState" value="${expectedViewState}">`
+      );
+    const actualViewState = await svairDataProvider.getViewState();
+    expect(actualViewState).toEqual(expectedViewState);
   });
 });
