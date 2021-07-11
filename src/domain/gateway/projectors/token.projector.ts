1ea1be79699688c23bd2ff0c9520ab27f3ce5468
Stanislas Bernard <bernardstanislas@gmail.com>
2021-07-11 22:26:54 +0200

    :sparkles: Token projector

---
+++ src/domain/gateway/projectors/token.projector.ts
@@ -0,0 +1,19 @@
+import {ApplicationId} from 'src/domain/gateway/application-id';
+import {ApplicationCreated} from 'src/domain/gateway/events/application-created.event';
+import {Token} from 'src/domain/gateway/projections/token';
+import {TokenRepository} from 'src/domain/gateway/repositories/token.repository';
+
+export class TokenProjector {
+  constructor(private readonly tokenRepository: TokenRepository) {}
+
+  onApplicationCreated(event: ApplicationCreated): void {
+    const newToken = new Token(
+      event.aggregateId as ApplicationId,
+      event.tokenValue,
+      event.scopes,
+      event.subscriptions
+    );
+
+    this.tokenRepository.save(newToken);
+  }
+}
--- src/domain/gateway/repositories/token.repository.ts
+++ src/domain/gateway/repositories/token.repository.ts
@@ -3,4 +3,6 @@ import {TokenValue} from 'src/domain/gateway/token-value';
 
 export interface TokenRepository {
   findByTokenValue(tokenValue: TokenValue): Promise<Token>;
+
+  save(token: Token): void;
 }
