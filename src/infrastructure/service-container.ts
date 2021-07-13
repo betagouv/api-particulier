0316b7e92263907d13563061e55005113baa609e
Stanislas Bernard <bernardstanislas@gmail.com>
2021-07-13 17:34:03 +0200

    :sparkles: Service container

---
+++ src/infrastructure/service-container.ts
@@ -0,0 +1,35 @@
+import {Client} from 'pg';
+import * as IORedis from 'ioredis';
+import {PostgresEventStore} from 'src/infrastructure/postgres.event-store';
+import {TokenRepositoryWithHashRetry} from 'src/domain/data-fetching/repositories/token-with-hash-retry.repository';
+import {RedisTokenRepository} from 'src/infrastructure/repositories/redis-token.repository';
+import {BullEventBus} from 'src/infrastructure/event-bus/bull.event-bus';
+import {DataProviderClient} from 'src/domain/data-fetching/data-provider-client';
+import {SoapDataProvider} from 'src/infrastructure/data-providers/cnaf/soap';
+import {SvairDataProvider} from 'src/infrastructure/data-providers/dgfip/svair';
+import {EventStore} from 'src/domain/event-store';
+import {TokenRepository} from 'src/domain/data-fetching/repositories/token.repository';
+import {EventBus} from 'src/domain/event-bus';
+
+const postgresClient = new Client(process.env.DATABASE_URL);
+
+export const eventStore: EventStore = new PostgresEventStore(postgresClient);
+
+const redisConnection = new IORedis({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+});
+
+export const tokenRepository: TokenRepository =
+  new TokenRepositoryWithHashRetry(new RedisTokenRepository(redisConnection));
+
+export const eventBus: EventBus = new BullEventBus(redisConnection);
+
+const cnafDataProvider = new SoapDataProvider();
+const dgfipDataProvider = new SvairDataProvider();
+
+export const dataProviderClient: DataProviderClient = new DataProviderClient(
+  cnafDataProvider,
+  dgfipDataProvider,
+  eventBus
+);
