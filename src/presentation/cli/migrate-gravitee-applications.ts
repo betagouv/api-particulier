564ff6064d1cd2d6a6a463de27f28027a44f5c8a
Stanislas Bernard <bernardstanislas@gmail.com>
2021-07-16 11:14:38 +0200

    :sparkles: Prepare apps for insertion

--- src/presentation/cli/migrate-gravitee-applications.ts
+++ src/presentation/cli/migrate-gravitee-applications.ts
@@ -14,7 +14,7 @@ const listAlreadyImportedApplicationIds = async (pg: Client) => {
 
 const getApplicationToImport = async (pg: Client) => {
   const result = await pg.query(`
-  SELECT app.id as application_id, app.name as application_name, app.description as application_description, scopes_md.value as scopes, legacy_md.value as legacy_token_hash, k.key as token, users.email as user_email
+  SELECT app.id as application_id, app.name as application_name, app.description as application_description, array_agg(scopes_md.value) as scopes, array_agg(legacy_md.value) as legacy_token_hash, array_agg(k.key) as token, array_agg(users.email) as user_email
   FROM applications app
   INNER JOIN metadata scopes_md ON scopes_md.reference_id = app.id AND scopes_md.key = 'scopes'
   LEFT OUTER JOIN metadata legacy_md ON legacy_md.reference_id = app.id AND legacy_md.key = 'legacy-token-hash'
@@ -22,19 +22,43 @@ const getApplicationToImport = async (pg: Client) => {
   INNER JOIN memberships ON memberships.reference_id = app.id
   INNER JOIN users ON users.id = memberships.member_id
   WHERE app.status = 'ACTIVE'
+  GROUP BY app.id
 `);
-  const applicationUsers = result.rows.reduce((acc, row) => {
-    if (!acc[row.application_id]) {
-      acc[row.application_id] = [];
+  return result.rows.map(row => {
+    const dataPassIdMatch = row.application_description.match(
+      /Numéro de demande : ([0-9]+)/
+    );
+    const dataPassId = dataPassIdMatch ? dataPassIdMatch[1] : '';
+
+    const scopes = row.scopes[0].split(',');
+
+    const subscriptions = [];
+    if (row.scopes[0].indexOf('dgfip') !== -1) {
+      subscriptions.push('DGFIP');
     }
-    if (
-      row.user_email !== null &&
-      acc[row.application_id].indexOf(row.user_email) === -1
-    ) {
-      acc[row.application_id].push(row.user_email);
+    if (row.scopes[0].indexOf('cnaf') !== -1) {
+      subscriptions.push('CNAF');
     }
-    return acc;
-  }, {});
+    const filteredLegacyTokenHash = row.legacy_token_hash.filter(
+      (hash: string) => hash !== null
+    );
+    const filteredToken = row.token.filter((token: string) => token !== null);
+    const tokenValue =
+      filteredLegacyTokenHash.length > 0
+        ? filteredLegacyTokenHash[0]
+        : filteredToken[0];
+    return {
+      applicationId: row.application_id,
+      applicationName: row.application_name.replace(/ - [0-9]+$/, ''),
+      dataPassId,
+      scopes,
+      subscriptions,
+      userEmails: Array.from(
+        new Set(row.user_email.filter((email: string) => email !== null))
+      ),
+      tokenValue,
+    };
+  });
 };
 
 (async () => {
