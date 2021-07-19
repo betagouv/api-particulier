8cd6649698540dbd527fa245600dc6af2c98075c
Stanislas Bernard <bernardstanislas@gmail.com>
2021-07-19 15:48:53 +0200

    :loud_sound: Log event publishing

--- src/infrastructure/event-bus/bull.event-bus.ts
+++ src/infrastructure/event-bus/bull.event-bus.ts
@@ -1,8 +1,10 @@
 import {Queue} from 'bullmq';
 import * as IORedis from 'ioredis';
 import {TokenConsumed} from 'src/domain/data-fetching/events/token-consumed.event';
+import {TokenNotFound} from 'src/domain/data-fetching/events/token-not-found.event';
 import {Event} from 'src/domain/event';
 import {EventBus} from 'src/domain/event-bus';
+import {logFor} from 'src/domain/logger';
 
 export const applicationEventQueueName = 'Application events';
 export const tokenEventQueueName = 'Token events';
@@ -10,6 +12,7 @@ export const tokenEventQueueName = 'Token events';
 export class BullEventBus implements EventBus {
   private readonly applicationEventQueue: Queue;
   private readonly tokenEventQueue: Queue;
+  private readonly logger = logFor(BullEventBus.name);
 
   constructor(redisConnection: IORedis.Redis) {
     this.applicationEventQueue = new Queue(applicationEventQueueName, {
@@ -21,7 +24,11 @@ export class BullEventBus implements EventBus {
   }
 
   publish(event: Event): void {
+    this.logger.log('debug', `Publishing ${event.constructor.name} event`, {
+      event,
+    });
     switch (event.constructor) {
+      case TokenNotFound:
       case TokenConsumed:
         this.tokenEventQueue.add(event.constructor.name, event);
         break;
