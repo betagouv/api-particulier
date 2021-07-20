ffe19c0497ce37f04db4d4c6ac503cd4a1375abe
Stanislas Bernard <bernardstanislas@gmail.com>
2021-07-20 11:32:29 +0200

    :sparkles: Scopes filters now works with nested attributes

--- src/domain/data-fetching/scopes-filters/property-based.scopes-filter.test.ts
+++ src/domain/data-fetching/scopes-filters/property-based.scopes-filter.test.ts
@@ -4,14 +4,19 @@ import {
 } from './property-based.scopes-filter';
 
 describe('The generic property-based scopes filter', () => {
-  type TestScope = 'yolo' | 'croute';
+  type TestScope = 'yolo' | 'croute' | 'moustaki';
   const payload = {
     name: 'Jean',
     surname: 'Croute',
+    birthdate: {
+      day: 3,
+      year: 1991,
+    },
   };
   const scopesConfiguration: ScopesConfiguration<TestScope, typeof payload> = {
     yolo: ['name'],
     croute: ['surname'],
+    moustaki: ['birthdate.day'],
   };
   const {filter} = new PropertyBasedScopesFilter(scopesConfiguration);
 
@@ -23,5 +28,13 @@ describe('The generic property-based scopes filter', () => {
 
   it('returns an empty object when no scope is provided', () => {
     expect(filter([], payload)).toEqual({});
+  });
+
+  it('filters nested properties', () => {
+    expect(filter(['moustaki'], payload)).toEqual({
+      birthdate: {
+        day: payload.birthdate.day,
+      },
+    });
   });
 });
--- src/domain/data-fetching/scopes-filters/property-based.scopes-filter.ts
+++ src/domain/data-fetching/scopes-filters/property-based.scopes-filter.ts
@@ -1,11 +1,12 @@
 import * as _ from 'lodash';
+import {pick} from 'lodash';
 import {logFor} from 'src/domain/logger';
 import {DataProviderResponse} from '../data-providers/dto';
 
 export type ScopesConfiguration<
   Scope extends string,
   T extends DataProviderResponse
-> = Record<Scope, (keyof T)[]>;
+> = Record<Scope, string[]>;
 
 export class PropertyBasedScopesFilter<
   Scope extends string,
@@ -18,18 +19,14 @@ export class PropertyBasedScopesFilter<
   ) {}
 
   filter = (scopes: Scope[], response: Partial<T>): Partial<T> => {
-    const maskedProperties: (keyof T)[] = _(
+    const maskedProperties: string[] = _(
       scopes.map(scope => this.scopesConfiguration[scope])
     )
       .flattenDeep()
       .uniq()
       .value();
-    const filteredResponse = Object.keys(response).reduce((result, key) => {
-      if (!maskedProperties.includes(key as keyof T)) {
-        return result;
-      }
-      return {...result, [key]: response[key as keyof T]};
-    }, {} as T);
+
+    const filteredResponse = pick(response, maskedProperties);
 
     this.logger.log('debug', 'Filtered response', {
       scopes,
