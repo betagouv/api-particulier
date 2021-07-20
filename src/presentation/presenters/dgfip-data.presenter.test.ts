dd119423ca8b72c46eca9bb74e2d7c6adf81ab20
Stanislas Bernard <bernardstanislas@gmail.com>
2021-07-20 10:31:02 +0200

    :sparkles: Dgfip data presenter

---
+++ src/presentation/presenters/dgfip-data.presenter.test.ts
@@ -0,0 +1,36 @@
+import {DgfipOutput} from 'src/domain/data-fetching/data-providers/dgfip/dto';
+import {DgfipDataPresenter} from 'src/presentation/presenters/dgfip-data.presenter';
+
+describe('The DGFIP data presenter', () => {
+  const dgfipDataPresenter = new DgfipDataPresenter();
+  it('replaces undefined values with empty strings', () => {
+    const input: Partial<DgfipOutput> = {
+      declarant1: {
+        nom: 'Croute',
+        prenoms: 'Jean',
+        dateNaissance: new Date('1991-05-03'),
+      },
+      anneeImpots: 2020,
+      anneeRevenus: 2021,
+      nombreParts: 2,
+      erreurCorrectif: undefined,
+    };
+
+    const output = dgfipDataPresenter.presentData(input, false);
+
+    expect(output.erreurCorrectif).toEqual('');
+    expect(output.montantImpot).toBeUndefined();
+  });
+
+  it('sets null values when needed', () => {
+    const input: Partial<DgfipOutput> = {
+      erreurCorrectif: undefined,
+      montantImpot: undefined,
+    };
+
+    const output = dgfipDataPresenter.presentData(input, true);
+
+    expect(output.erreurCorrectif).toEqual('');
+    expect(output.montantImpot).toBeNull();
+  });
+});
---
+++ src/presentation/presenters/dgfip-data.presenter.ts
@@ -0,0 +1,38 @@
+import {defaults, pick} from 'lodash';
+import {DgfipOutput} from 'src/domain/data-fetching/data-providers/dgfip/dto';
+
+export class DgfipDataPresenter {
+  presentData(input: Partial<DgfipOutput>, withNulls: boolean) {
+    const mask = {
+      declarant1: {
+        nom: '',
+        nomNaissance: '',
+        prenoms: '',
+        dateNaissance: '',
+      },
+      declarant2: {
+        nom: '',
+        nomNaissance: '',
+        prenoms: '',
+        dateNaissance: '',
+      },
+      foyerFiscal: {
+        adresse: '',
+      },
+      dateRecouvrement: '',
+      dateEtablissement: '',
+      nombreParts: '',
+      situationFamille: '',
+      nombrePersonnesCharge: 0,
+      revenuBrutGlobal: withNulls ? null : undefined,
+      revenuImposable: withNulls ? null : undefined,
+      impotRevenuNetAvantCorrections: withNulls ? null : undefined,
+      montantImpot: withNulls ? null : undefined,
+      revenuFiscalReference: withNulls ? null : undefined,
+      erreurCorrectif: '',
+      situationPartielle: '',
+    };
+
+    return defaults(input, pick(mask, Object.keys(input)));
+  }
+}
