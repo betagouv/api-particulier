c9e26a0dc7b6eb54a5013a4655f9f99f919fd483
Stanislas Bernard <bernardstanislas@gmail.com>
2021-07-20 11:10:36 +0200

    :wrench: Configure DGFIP scopes

--- src/domain/data-fetching/data-providers/dgfip/scopes.ts
+++ src/domain/data-fetching/data-providers/dgfip/scopes.ts
@@ -5,6 +5,30 @@ export type DgfipScope = 'dgfip_avis_imposition' | 'dgfip_adresse';
 
 export const scopesConfiguration: ScopesConfiguration<DgfipScope, DgfipOutput> =
   {
-    dgfip_avis_imposition: [],
-    dgfip_adresse: [],
+    dgfip_avis_imposition: [
+      'anneeImpots',
+      'anneeRevenus',
+      'dateEtablissement',
+      'dateRecouvrement',
+      'declarant1',
+      'declarant2',
+      'erreurCorrectif',
+      'foyerFiscal',
+      'impotRevenuNetAvantCorrections',
+      'montantImpot',
+      'nombreParts',
+      'nombrePersonnesCharge',
+      'revenuBrutGlobal',
+      'revenuFiscalReference',
+      'revenuImposable',
+      'situationFamille',
+      'situationPartielle',
+    ],
+    dgfip_adresse: [
+      'declarant1',
+      'declarant2',
+      'foyerFiscal',
+      'anneeImpots',
+      'anneeRevenus',
+    ],
   };
