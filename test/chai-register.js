50f452bfd56da02f23f694f6e2516e85c265e791
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-02 22:43:02 +0200

    :sparkles: Assertions on promise

--- test/chai-register.js
+++ test/chai-register.js
@@ -1,5 +1,7 @@
 /* eslint-disable node/no-unpublished-require */
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const sinonChai = require('sinon-chai');
 
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
