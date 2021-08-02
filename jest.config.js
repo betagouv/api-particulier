5f59185c55e801f84826cd7ed5abf3c37bd919ad
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-02 19:36:11 +0200

    :wrench: Jest ignores .spec files

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   clearMocks: true,
   testPathIgnorePatterns: ['/node_modules/', '/build/', '/test/'],
+  testMatch: ['<rootDir>/**/*.test.ts'],
   moduleNameMapper: {
     'src/(.*)': '<rootDir>/src/$1',
     'test/(.*)': '<rootDir>/test/$1',
