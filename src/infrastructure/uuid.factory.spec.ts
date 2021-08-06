eebd90ce50745f294b71f5669f5382199a419e59
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-06 11:04:15 +0200

    :alembic: Try coverage with mocha to coveralls

---
+++ src/infrastructure/uuid.factory.spec.ts
@@ -0,0 +1,13 @@
+import {expect} from 'chai';
+import {UuidFactory} from 'src/infrastructure/uuid.factory';
+
+describe('The uuid factory', () => {
+  it('generates uuids', () => {
+    const uuidFactory = new UuidFactory();
+
+    const firstUuid = uuidFactory.generateUuid();
+    const secondUuid = uuidFactory.generateUuid();
+
+    expect(firstUuid).not.to.equal(secondUuid);
+  });
+});
