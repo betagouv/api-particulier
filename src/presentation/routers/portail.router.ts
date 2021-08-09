53956b7e0d540e2ff69927e3771a081f59be629b
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-09 16:42:14 +0200

    :sparkles: Login

--- src/presentation/routers/portail.router.ts
+++ src/presentation/routers/portail.router.ts
@@ -2,6 +2,10 @@ import express, {Express, Router} from 'express';
 import expressHandlebars from 'express-handlebars';
 import path from 'path';
 import {listApplications} from 'src/presentation/controllers/list-applications.controller';
+import {Issuer, Strategy, TokenSet} from 'openid-client';
+import passport from 'passport';
+import session from 'express-session';
+import {UserEmail} from 'src/domain/application-management/user';
 
 export const initPortail = (app: Express) => {
   app.engine('handlebars', expressHandlebars());
@@ -10,6 +14,48 @@ export const initPortail = (app: Express) => {
   app.use(express.static('public'));
 };
 
+Issuer.discover(process.env.ISSUER_URL!).then(issuer => {
+  const client = new issuer.Client({
+    client_id: process.env.CLIENT_ID!,
+    client_secret: process.env.CLIENT_SECRET!,
+    redirect_uris: [process.env.REDIRECT_URL!],
+    response_types: ['code'],
+  });
+  passport.use(
+    'openid',
+    new Strategy(
+      {
+        client,
+        params: {
+          scope: 'openid email profile',
+        },
+      },
+      (_token: TokenSet, userInfo: object, done: Function) => {
+        done(null, userInfo);
+      }
+    )
+  );
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user as Express.User);
+});
+
 export const portailRouter = Router();
 
+portailRouter.use(session({secret: process.env.SESSION_SECRET!}));
+portailRouter.use(passport.initialize());
+portailRouter.use(passport.session());
 portailRouter.get('/', listApplications);
+portailRouter.get('/login', passport.authenticate('openid'));
+portailRouter.get(
+  '/callback',
+  passport.authenticate('openid', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  })
+);
