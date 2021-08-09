0b798369b191dfd3d2848dd090ca4f2861ae259a
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-09 10:58:02 +0200

    :sparkles: Fullstack build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
+const gulpEsbuild = require('gulp-esbuild');
 
 const css = () => {
   return gulp
@@ -8,12 +9,24 @@ const css = () => {
     .pipe(gulp.dest('./public'));
 };
 
-exports.css = css;
+const js = () => {
+  return gulp.src('./src/presentation/frontend/index.ts').pipe(
+    gulpEsbuild({
+      outfile: 'main.js',
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+    }).pipe(gulp.dest('./public'))
+  );
+};
 
-exports.cssDev = () => {
+exports.dev = () => {
   gulp.watch(
     'src/presentation/frontend/index.css',
     {ignoreInitial: false},
     css
   );
+  gulp.watch('src/presentation/frontend/index.ts', {ignoreInitial: false}, js);
 };
+
+exports.build = gulp.parallel(js, css);
