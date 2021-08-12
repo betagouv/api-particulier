704e6686ff3712d0bd1e197f2e3938d6583b11a4
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-12 23:19:46 +0200

    :sparkles: Is admin middleware

---
+++ src/presentation/middlewares/is-admin.middleware.test.ts
@@ -0,0 +1,53 @@
+import {expect} from 'chai';
+import {Request, Response} from 'express';
+import {stub} from 'sinon';
+import {UserEmail} from 'src/domain/application-management/user';
+import {isAdminMiddleware} from 'src/presentation/middlewares/is-admin.middleware';
+import {stubInterface} from 'ts-sinon';
+
+describe('The admin checker middleware', () => {
+  const adminEmail = <UserEmail>'georges@moustaki.fr';
+  process.env.ADMIN_USERS = adminEmail;
+
+  it('calls next when the logged in user is an admin', () => {
+    const res = stubInterface<Response>();
+    const next = stub();
+    const req = <Request>{
+      user: {
+        email: adminEmail,
+      },
+    };
+
+    isAdminMiddleware(req, res, next);
+
+    expect(next).to.have.been.calledOnceWithExactly();
+  });
+
+  it('returns a 403 when the logged in user is not an admin', () => {
+    const res = stubInterface<Response>();
+    const next = stub();
+    const req = <Request>{
+      user: {
+        email: 'jean@lol.fr',
+      },
+    };
+    res.status.returnsThis();
+
+    isAdminMiddleware(req, res, next);
+
+    expect(next).not.to.have.been.called;
+    expect(res.status).to.have.been.calledWith(403);
+  });
+
+  it('returns a 403 when there is no logged in user', () => {
+    const res = stubInterface<Response>();
+    const next = stub();
+    const req = <Request>{};
+    res.status.returnsThis();
+
+    isAdminMiddleware(req, res, next);
+
+    expect(next).not.to.have.been.called;
+    expect(res.status).to.have.been.calledWith(403);
+  });
+});
---
+++ src/presentation/middlewares/is-admin.middleware.ts
@@ -0,0 +1,19 @@
+import {Request, Response, NextFunction} from 'express';
+
+export const isAdminMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const userIsAdmin =
+    req.user &&
+    req.user.email &&
+    process.env.ADMIN_USERS &&
+    process.env.ADMIN_USERS.split(',').includes(req.user.email);
+  if (userIsAdmin) {
+    next();
+    return;
+  }
+
+  res.status(403).send('Forbidden');
+};
