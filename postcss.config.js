90376ff202b336bcd01ed5642c928a634a4ef96d
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-13 11:41:32 +0200

    :bug: Fix css purge

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: ['./src/presentation/frontend/views/**/*.handlebars'],
+  content: ['./src/presentation/frontend/views/**/*.njk'],
   // This is the function used to extract class names from your templates
   defaultExtractor: content => {
     // Capture as liberally as possible, including things like `h-(screen-1.5)`
