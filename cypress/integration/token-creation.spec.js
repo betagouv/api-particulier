266f1f06f412ffa5c6a9e33c1b416e4737ecf65f
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-19 09:04:36 +0200

    :white_check_mark: Get a token and use it to call the API

--- cypress/integration/token-creation.spec.js
+++ cypress/integration/token-creation.spec.js
@@ -1,6 +1,7 @@
 const {describe, it} = require('mocha');
+const qs = require('qs');
 
-describe('The portail', () => {
+describe('API Particulier', () => {
   it('Clears the auth cookies', () => {
     const issuerUrl = Cypress.env('ISSUER_URL');
     // eslint-disable-next-line node/no-unsupported-features/node-builtins
@@ -15,14 +16,16 @@ describe('The portail', () => {
     cy.contains('Cannot GET /');
   });
 
-  it('Is exposed on the portail domain', () => {
+  it('Is exposed on the portail domain with no token by default', () => {
     cy.visit(Cypress.env('BASE_URL'), {failOnStatusCode: false});
     cy.get('input[name="login"]').type(Cypress.env('TEST_OIDC_EMAIL'));
     cy.get('input[name="password"]').type(Cypress.env('TEST_OIDC_PASSWORD'));
     cy.get('form').submit();
 
     cy.contains("Vous n'avez pas de jeton associé à votre email");
+  });
 
+  it('can be called by DataPass', () => {
     cy.request({
       method: 'POST',
       url: 'http://localhost:3000/api/applications',
@@ -37,8 +40,32 @@ describe('The portail', () => {
         'X-Api-Key': Cypress.env('DATAPASS_API_KEY'),
       },
     });
+  });
 
+  it('lists the tokens', () => {
     cy.visit(Cypress.env('BASE_URL'), {failOnStatusCode: false});
+    cy.get('input[name="login"]').type(Cypress.env('TEST_OIDC_EMAIL'));
+    cy.get('input[name="password"]').type(Cypress.env('TEST_OIDC_PASSWORD'));
+    cy.get('form').submit();
+
     cy.contains('Application de test');
+
+    cy.get('.select-all').then(tokenElement => {
+      const token = tokenElement.text();
+      cy.request({
+        method: 'GET',
+        url: `http://localhost:3000/api/v2/situations-pole-emploi?${qs.stringify(
+          {
+            identifiant: Cypress.env('TEST_POLE_EMPLOI_ID'),
+          }
+        )}`,
+
+        headers: {
+          'X-Api-Key': token,
+        },
+      }).then(response => {
+        expect(response.status).to.eq(200);
+      });
+    });
   });
 });
