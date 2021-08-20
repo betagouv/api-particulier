852b3fc319b1d424f53e8dcab3ac6d4461713259
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-20 08:39:29 +0200

    :rotating_light: Remove unused import

--- src/domain/data-fetching/projectors/token.projector.ts
+++ src/domain/data-fetching/projectors/token.projector.ts
@@ -4,7 +4,6 @@ import {ApplicationImported} from 'src/domain/application-management/events/appl
 import {Token} from 'src/domain/data-fetching/projections/token';
 import {TokenRepository} from 'src/domain/data-fetching/repositories/token.repository';
 import {logFor, Logger} from 'src/domain/logger';
-import {eventBus} from 'src/infrastructure/service-container';
 
 export class TokenProjector {
   private readonly logger: Logger;
