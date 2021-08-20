d373c579a37a111c6b5b207d34340ad31332f836
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-20 10:31:05 +0200

    :poop: Wait 3 seconds for projections to be done

--- src/presentation/cli/publish-all-events.ts
+++ src/presentation/cli/publish-all-events.ts
@@ -2,16 +2,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import {Command} from 'commander';
 import {postgresPool} from 'src/infrastructure/configuration/postgres';
-import {PostgresEventStore} from 'src/infrastructure/postgres.event-store';
-import {EventEmitterEventBus} from 'src/infrastructure/event-bus/event-emitter.event-bus';
+import {eventBus, eventStore} from 'src/infrastructure/service-container';
 
 (async () => {
   const program = new Command();
   program.version('0.0.1');
   program.parse(process.argv);
 
-  const eventStore = new PostgresEventStore(postgresPool);
-  const eventBus = new EventEmitterEventBus();
   const events = await eventStore.listEvents();
 
   console.log('Removing projections');
@@ -21,14 +18,14 @@ import {EventEmitterEventBus} from 'src/infrastructure/event-bus/event-emitter.e
 
   console.log(`Publishing ${events.length} events`);
 
-  await Promise.all(
-    events.map(event => {
-      return eventBus.publish(event);
-    })
-  );
+  events.forEach(event => {
+    eventBus.publish(event);
+  });
 
-  await postgresPool.end();
-  console.log('Disconnected');
-  // eslint-disable-next-line no-process-exit
-  process.exit(0);
+  setTimeout(async () => {
+    await postgresPool.end();
+    console.log('Disconnected');
+    // eslint-disable-next-line no-process-exit
+    process.exit(0);
+  }, 3000);
 })();
