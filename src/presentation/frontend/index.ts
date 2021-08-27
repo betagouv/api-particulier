44eb47035455f23caaa4662946edab9faf148ea0
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-27 15:59:30 +0200

    :sparkles: Display form on button click

--- src/presentation/frontend/index.ts
+++ src/presentation/frontend/index.ts
@@ -1,5 +1,10 @@
 import {Application} from 'stimulus';
+import feather from 'feather-icons';
 import ClipboardController from './clipboard.controller';
+import NewApplicationController from './new-application.controller';
+
+feather.replace();
 
 const application = Application.start();
 application.register('clipboard', ClipboardController);
+application.register('new-application', NewApplicationController);
