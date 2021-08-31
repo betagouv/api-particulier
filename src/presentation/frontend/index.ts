19e79c3f23ff77fb4949cbad526d21a09631691e
Stanislas Bernard <bernardstanislas@gmail.com>
2021-08-31 14:54:06 +0000

    :sparkles: Display scopes

--- src/presentation/frontend/index.ts
+++ src/presentation/frontend/index.ts
@@ -1,11 +1,11 @@
 import {Application} from 'stimulus';
 import feather from 'feather-icons';
-import ClipboardController from './clipboard.controller';
+import ApplicationController from './application.controller';
 import NewApplicationController from './new-application.controller';
 import '@hotwired/turbo';
 
 window.feather = feather;
 
 const application = Application.start();
-application.register('clipboard', ClipboardController);
+application.register('application', ApplicationController);
 application.register('new-application', NewApplicationController);
