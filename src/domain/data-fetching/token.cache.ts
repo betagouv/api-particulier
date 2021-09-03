de86214322190d4c774cefbae94ead877a229f11
Stanislas Bernard <bernardstanislas@gmail.com>
2021-09-03 09:56:55 +0000

    :sparkles: Add token cache

---
+++ src/domain/data-fetching/token.cache.ts
@@ -0,0 +1,17 @@
+import {memoize, MemoizedFunction} from 'lodash';
+import {Token} from 'src/domain/data-fetching/projections/token';
+import {TokenRepository} from 'src/domain/data-fetching/repositories/token.repository';
+import {TokenValue} from 'src/domain/token-value';
+
+export class TokenCache {
+  readonly findByTokenValue: ((tokenValue: TokenValue) => Promise<Token>) &
+    MemoizedFunction;
+
+  constructor(tokenRepository: TokenRepository) {
+    this.findByTokenValue = memoize(tokenRepository.findByTokenValue);
+  }
+
+  clear() {
+    this.findByTokenValue.cache.clear!();
+  }
+}
