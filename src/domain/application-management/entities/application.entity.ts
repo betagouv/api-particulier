0efa44caad5eca8284dd6f623273d7e707d5d0e6
Stanislas Bernard <bernardstanislas@gmail.com>
2021-10-11 10:13:36 +0000

    :recycle: Create mirror entity for Application aggregate

---
+++ src/domain/application-management/entities/application.entity.ts
@@ -0,0 +1,82 @@
+import {ApplicationId} from 'src/domain/application-id';
+import {TokenValueFactory} from 'src/domain/application-management/token-value.factory';
+import {UserEmail} from 'src/domain/application-management/user';
+import {logFor} from 'src/domain/logger';
+import {AnyScope} from 'src/domain/scopes';
+import {Subscription} from 'src/domain/subscription';
+import {TokenValue} from 'src/domain/token-value';
+import {UuidFactory} from 'src/domain/uuid.factory';
+
+export type Token = {
+  value: TokenValue;
+  createdOn: Date;
+};
+
+export class Application {
+  private readonly logger = logFor(Application.name);
+
+  private constructor(
+    public readonly id: ApplicationId,
+    public readonly name: string,
+    public readonly createdOn: Date,
+    public readonly dataPassId: string,
+    public readonly tokens: Token[],
+    public readonly subscriptions: Subscription[],
+    public readonly userEmails: UserEmail[],
+    public readonly scopes: AnyScope[]
+  ) {}
+
+  static create(
+    name: string,
+    dataPassId: string,
+    subscriptions: Subscription[],
+    scopes: AnyScope[],
+    userEmails: UserEmail[],
+    uuidFactory: UuidFactory,
+    tokenValueFactory: TokenValueFactory
+  ): Application {
+    return new this(
+      uuidFactory.generateUuid() as ApplicationId,
+      name,
+      new Date(),
+      dataPassId,
+      [
+        {
+          value: tokenValueFactory.generateTokenValue(),
+          createdOn: new Date(),
+        },
+      ],
+      subscriptions,
+      userEmails,
+      scopes
+    );
+  }
+
+  static import(
+    id: ApplicationId,
+    name: string,
+    dataPassId: string,
+    subscriptions: Subscription[],
+    scopes: AnyScope[],
+    userEmails: UserEmail[],
+    tokenValues: TokenValue[]
+  ): Application {
+    return new this(
+      id,
+      name,
+      new Date(),
+      dataPassId,
+      tokenValues.map(tokenValue => ({
+        createdOn: new Date(),
+        value: tokenValue,
+      })),
+      subscriptions,
+      userEmails,
+      scopes
+    );
+  }
+
+  subscribeUser(userEmail: UserEmail) {
+    this.userEmails.push(userEmail);
+  }
+}
