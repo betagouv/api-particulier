248a991ef4ccde221913b5083be634d3862ab8fd
Stanislas Bernard <bernardstanislas@gmail.com>
2022-02-14 12:09:15 +0100

    :sparkles: FranceConnect CNOUS usecase

--- src/application/usecases/fetch-data-with-france-connect.usecase.ts
+++ src/application/usecases/fetch-data-with-france-connect.usecase.ts
@@ -1,5 +1,6 @@
 import {setUser} from '@sentry/node';
 import {DataProviderClient} from 'src/domain/data-fetching/data-provider-client';
+import {CnousInput} from 'src/domain/data-fetching/data-providers/cnous/dto';
 import {MesriInput} from 'src/domain/data-fetching/data-providers/mesri/dto';
 import {FranceConnectClient} from 'src/domain/data-fetching/france-connect.client';
 import {Token} from 'src/domain/data-fetching/projections/token';
@@ -16,13 +17,11 @@ export class FetchDataWithFranceConnectUsecase {
     setCurrentToken: (token: Token) => void,
     setInputData: (inputData: unknown) => void
   ) {
-    const {identity, token} =
-      await this.franceConnectClient.getTokenAndIdentityFromAccessToken(
-        accessToken
-      );
-    setUser({id: token.application.id});
-    setCurrentToken(token);
-    setInputData(identity);
+    const {identity, token} = await this.prepareInputWithFranceConnect(
+      accessToken,
+      setCurrentToken,
+      setInputData
+    );
 
     const mesriInput: MesriInput = {
       dateNaissance: new Date(identity.birthdate),
@@ -32,5 +31,42 @@ export class FetchDataWithFranceConnectUsecase {
       lieuNaissance: identity.birthplace,
     };
     return this.dataProviderClient.consumeMesri(mesriInput, token);
+  }
+
+  async fetchCnousData(
+    accessToken: TokenValue,
+    setCurrentToken: (token: Token) => void,
+    setInputData: (inputData: unknown) => void
+  ) {
+    const {identity, token} = await this.prepareInputWithFranceConnect(
+      accessToken,
+      setCurrentToken,
+      setInputData
+    );
+
+    const cnousInput: CnousInput = {
+      dateNaissance: new Date(identity.birthdate),
+      nomFamille: identity.family_name,
+      prenoms: identity.given_name,
+      sexe: identity.gender === 'female' ? '2' : '1',
+      lieuNaissance: identity.birthplace,
+    };
+    return this.dataProviderClient.consumeCnous(cnousInput, token);
+  }
+
+  private async prepareInputWithFranceConnect(
+    accessToken: TokenValue,
+    setCurrentToken: (token: Token) => void,
+    setInputData: (inputData: unknown) => void
+  ) {
+    const {identity, token} =
+      await this.franceConnectClient.getTokenAndIdentityFromAccessToken(
+        accessToken
+      );
+    setUser({id: token.application.id});
+    setCurrentToken(token);
+    setInputData(identity);
+
+    return {identity, token};
   }
 }
